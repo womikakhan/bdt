1cdb06bfbda48efc003fd21f2895714b3cb4eeb1
Womika khan <76390845+womikakhan@users.noreply.github.com>
2022-06-24 22:33:32 +0530

    Create pattern.scala

---
+++ pattern.scala
@@ -0,0 +1,18 @@
+abstract class Notification
+
+case class SMS(number:String, message: String) extends Notification
+case class Email(emailAddr: String, subject: String, body: String) extends Notification
+
+object test {
+  def showNotification(notification : Notification): String = {
+    case Email(emailAddr,subject,_) => s"You recieved an Email from $emailAddr with subject = $subject"
+    case SMS(number,msg) => s"You recieved a message from $number with message = $msg"
+  }
+  def main(agrs : Array[String]) : Unit = {
+    val sms = SMS("3768346845","Hello")
+    val email = Email("abc@gmail.com","test","hello")
+    
+    println(showNotification(sms))
+    println(showNotification(email))
+  }
+}
