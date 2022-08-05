c767e54ce303f6c387a7b772057790cceab6425e
Womika khan <76390845+womikakhan@users.noreply.github.com>
2022-08-05 20:40:37 +0530

    Update wordcount.scala

--- wordcount.scala
+++ wordcount.scala
@@ -14,3 +14,8 @@ object wordcount {
     highFreq.saveAsTextFile("wordcountsDir")
   }
 }
+
+name := "WordCount"
+version := "1.0"
+scalaVersion := "2.11.12"
+libraryDependencies += "org.apache.spark" %% "spark-core" % "2.3.0"
